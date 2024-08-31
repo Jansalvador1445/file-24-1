0ce203ac7bfc0a1c5cd397b1eaf87ca8d9e17345
Jan Salvador Sebastian <sebastian@mclinica.com>
2024-08-31 08:21:42 +0800

    Added some codes

--- Main.java
+++ Main.java
@@ -2,7 +2,9 @@ class Main {
 
     public static void main (String [] args) {
         String section = "Block #1";
+        String subject = "ITE 300 and SSP 005";
 
+        System.out.println(subject);
         System.out.println(section);
         
     }
